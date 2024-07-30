2823896176cfc82d81a0e8c562e771c9e6b623c8
mermaid <mermaid@ANASTASIASMEA3D>
2024-07-30 21:35:03 +0200

    Добавьте файлы проекта.

---
+++ Lesson17/BankAccaunt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    internal class BankAccaunt<T>
+    {
+        T Number { get; set; }
+        double Balanse { get; set; }
+        string Name { get; set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите значение текущего баланса");
+            Balanse = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца аккаунта");
+            Name = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"{Number} {Name} {Balanse}";
+        }
+    }
+}
---
+++ Lesson17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccaunt<int> bankAcc1 = new BankAccaunt<int>();
+            bankAcc1.Input();
+            Console.WriteLine(bankAcc1.GetInfo());
+
+            BankAccaunt<string> bankAcc2 = new BankAccaunt<string>();
+            bankAcc2.Input();
+            Console.WriteLine(bankAcc2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
